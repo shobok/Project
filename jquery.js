fd71e0b6fb196ecee45ea629a478b235b800a2b9
shobok <shob@ukr.net>
2016-03-05 17:37:06 +0200

    refactoring

--- jquery.js
+++ jquery.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+$(document).ready(function() {
+    $('a[href^="#"]').bind('click.smoothscroll',function(e) {
 		e.preventDefault();
 		var target = this.hash,
 		$target = $(target);
 		$('html, body').stop().animate({
 			'scrollTop': $target.offset().top
-		}, 1000, 'swing', function () {
+		}, 1000, 'swing', function() {
 			window.location.hash = target;
 		});
 	});
@@ -14,14 +14,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#to-top").hide();
 	$(function () {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#to-top').fadeIn();
         } else {
             $('#to-top').fadeOut();
         }
     });
-	   $('#to-top').click(function () {
+	   $('#to-top').click(function() {
 	   $('body,html').animate({scrollTop: 0}, 900);
 	   return false;
         });
